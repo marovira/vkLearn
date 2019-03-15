3085021625884a427b5d3a5f9696481f57d74ca5
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-03-14 21:49:38 -0700

    [brief] Adds clang-format.
    [detailed]

--- source/vk_test/main.cpp
+++ source/vk_test/main.cpp
@@ -1,12 +1,6 @@
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
-
-#include <fmt/printf.h>
-
 #define GLFW_INCLUDE_VULKAN
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
+#include <fmt/printf.h>
 #include <vulkan/vulkan.hpp>
 
 int main()
